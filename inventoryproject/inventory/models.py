af5c33722b585e425c4c0ef7565ac6850d6b2c59
Paul.Anderson <Paul.Anderson@fdmgroup.com>
2018-12-06 16:22:08 +0000

    added webserver test passing and webserver model

--- inventoryproject/inventory/models.py
+++ inventoryproject/inventory/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Webserver(models.Model):
+    name = models.CharField(max_length=50)
+    vendor = models.CharField(max_length=50)
+    patch_level = models.CharField(max_length=50)
--- inventoryproject/inventory/tests.py
+++ inventoryproject/inventory/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+import pytest 
+from inventory.models import Webserver
+import unittest
 
-# Create your tests here.
+class Test_Webserver_Model:
+
+    @pytest.mark.django_db
+    def test_webserver_data_is_saved(self):
+        webserver =  Webserver.objects.create(name="webserver_1", vendor="Apache", patch_level="Q2-2016")
+        assert webserver.name == "webserver_1"
+        assert webserver.vendor == "Apache"
+        assert webserver.patch_level == "Q2-2016"
