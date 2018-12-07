41103f44f5834fba23c2ac2f9e9eb42266a93d2a
ayewhy <u1452160@unimail.hud.ac.uk>
2018-12-07 10:48:25 +0000

    in_use added and test updated

--- inventoryproject/inventory/tests.py
+++ inventoryproject/inventory/tests.py
@@ -6,7 +6,8 @@ class Test_Webserver_Model:
 
     @pytest.mark.django_db
     def test_webserver_data_is_saved(self):
-        webserver =  Webserver.objects.create(name="webserver_1", vendor="Apache", patch_level="Q2-2016")
+        webserver =  Webserver.objects.create(name="webserver_1", vendor="Apache", patch_level="Q2-2016",in_use=False)
         assert webserver.name == "webserver_1"
         assert webserver.vendor == "Apache"
         assert webserver.patch_level == "Q2-2016"
+        assert webserver.in_use == False 
