16e9f04f88ea5f7be14f919e3e3ffaa4d82f696f
Charlie Maunder <charlie.maunder@fdmgroup.com>
2018-12-10 11:39:37 +0000

    added a complete form.py file

---
+++ inventoryproject/inventory/form.py
@@ -0,0 +1,17 @@
+from django import forms
+from inventory.models import Webserver, Database, Host
+
+class WebserverForm(forms.ModelForm):
+    class Meta:
+        model = Webserver
+        fields = ['name','vendor','patch_level']
+class DatabaseForm(forms.ModelForm):
+    class Meta:
+        model = Database
+        fields = ['name','vendor','patch_level']
+
+class Host(forms.ModelForm):
+    class Meta:
+        model = Host
+        fields = ['fqdn','os_type','os_patch_level','environment']
+        
