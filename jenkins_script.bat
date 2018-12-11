b6fd86910e8fdfcabf98ac233e2470b156dee936
mpatel500 <42186500+mpatel500@users.noreply.github.com>
2018-12-11 16:39:01 +0000

    Update jenkins_script.bat

--- jenkins_script.bat
+++ jenkins_script.bat
@@ -2,4 +2,3 @@ pip install -r requirements.txt
 cd inventoryproject
 pytest --junit-xml=pytest_xml.xml
 py.test --cov=inventory inventory/ --cov-report html --cov-report xml
-coverage run inventory/tests.py
