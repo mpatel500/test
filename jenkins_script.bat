76bf8215be784d2f848a51e6d38fe29de134e9ac
mpatel500 <42186500+mpatel500@users.noreply.github.com>
2018-12-11 15:28:49 +0000

    Update jenkins_script.bat

--- jenkins_script.bat
+++ jenkins_script.bat
@@ -2,9 +2,7 @@ pip install -r requirements.txt
 cd inventoryproject
 pytest --junit-xml=pytest_xml.xml
 py.test --cov=test test inventoryproject/inventory/tests
-coverage run pytest.ini
+pytest.ini
 coverage report -m
 coverage xml -o coverage_report.xml
 coverage html -d coverage
-
-
