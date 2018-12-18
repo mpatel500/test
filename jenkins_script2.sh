7b752c5716eb20227982ded59798864d67ca8953
mpatel500 <42186500+mpatel500@users.noreply.github.com>
2018-12-18 16:36:16 +0000

    Update jenkins_script2.sh

--- jenkins_script2.sh
+++ jenkins_script2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 source ./env/bin/activate
 pip freeze
-pip install -r requirements.txt
 sudo -H pip install -r requirements.txt
 cd inventoryproject
 #python manage.py runserver
 pytest --junit-xml=pytest_xml.xml
+pip freeze
 py.test --cov=inventory inventory/ --cov-report html --cov-report xml
