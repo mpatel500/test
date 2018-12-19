41143dddb923bac022a76d85e71dc7fe382955fe
mpatel500 <42186500+mpatel500@users.noreply.github.com>
2018-12-19 15:42:13 +0000

    Update jenkins_script2.sh

--- jenkins_script2.sh
+++ jenkins_script2.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 source ./env/bin/activate
+echo "PIP FREEZE BEFORE REQUIREMENTS.TXT IS INSTALLED"
 pip freeze
 sudo -H pip install -r requirements.txt
+echo "PIP FREEZE AFTER REQUIREMENTS.TXT IS INSTALLED"
+pip freeze
 cd inventoryproject
 #python manage.py runserver
 pytest --junit-xml=pytest_xml.xml
-pip freeze
+
 py.test --cov=inventory inventory/ --cov-report html --cov-report xml
 deactivate
